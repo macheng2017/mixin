5118afeb33988c0a870eab2670afc95a80e87799
macheng2017 <gos360@163.com>
2018-06-03 18:41:32 +0800

    修改错误,未改完

--- src/store/store.js
+++ src/store/store.js
@@ -8,21 +8,25 @@ var store = new vuex.Store({
 		password : '1254'
 	},
 	actions : {
-		action_pw(context,payload){
+	async	action_pw(context,payload){
 			var pw = ''
 			axios.get('/x/web-interface/ranking/region?rid=1&day=7&jsonp=jsonp').then((res)=>{
 
 			  console.log(res)
 			},(err)=>{});
-			axios.get('http://45.32.51.110:1234/set_a_mima').then((res)=>{
-			    console.log(res)
-			    pw = res.mima
-			},(err)=>{});		
-			console.log(1)	
-			axios.get('/set_a_mima').then((res)=>{
-			    console.log(res)
-			    pw = res.mima
-			},(err)=>{});
+			// axios.get('http://45.32.51.110:1234/set_a_mima').then((res)=>{
+			//     console.log('----------' + JSON.stringify(res))
+			//     pw = res.data.mima
+      // },(err)=>{});
+
+      let res = await axios.get('http://45.32.51.110:1234/set_a_mima')
+      pw =  res.data.mima
+			console.log(1)
+			// axios.get('/set_a_mima').then((res)=>{
+			//     console.log(res)
+			//     pw = res.mima
+      // },(err)=>{});
+      console.log('-----------' + pw);
 			context.commit('CHANGE_PASSWORD',pw);
 		}
 	},
